9a45af8b75d31ee050229da9a63e64664b962e07
Jonson26 <filipmj@op.pl>
2020-04-07 00:20:43 +0200

    *added new Bomb class in pieces.py *added bomb1.gif, bomb2.gif, bomb3.gif, bomb4.gif and bomb5.gif for dynamic texturing of the "Bomb" piece

--- pieces.py
+++ pieces.py
@@ -28,4 +28,38 @@ class Rook(Piece):
         for i in range(-10, 10):
             if(0!=i):
                 t.append((x, y + i))
+        return t    
+    
+class Bomb(Piece):
+    
+    def __init__(self, x, y, team, board):
+        super().__init__(x, y, team, board)   
+        self.timer=5
+        self.img = PhotoImage(file="img/bomb"+str(self.timer)+".gif")
+    
+    def getMoveTable(self):
+        x = self.x
+        y = self.y
+        t = []
+        for i in range(-10, 10):
+            if(0!=i):
+                t.append((x + i, y))
+        for i in range(-10, 10):
+            if(0!=i):
+                t.append((x, y + i))
+        for i in range(-10, 10):
+            if(0!=i):
+                t.append((x + i, y - i))
+                t.append((x + i, y + i))
         return t
+    
+    def getImg(self):
+        self.img = PhotoImage(file="img/bomb"+str(self.timer)+".gif")
+        return self.img
+    
+    def move(self, x, y):
+        self.super().move(x, y)
+        timer-=1
+        if(timer==0):
+            self.board.remove(self)
+        return True
--- piecetest1.py
+++ piecetest1.py
@@ -19,12 +19,19 @@ class BoardApp:
         f1.pack(side = TOP)
         
         b1 = Button(f1, text="Knight", command=self.piece1)
-        b2 = Button(f1, text="Rook", command=self.piece2)     
+        b2 = Button(f1, text="Rook", command=self.piece2)
+        b3 = Button(f1, text="Bomb", command=self.piece3)     
         b1.pack(side = LEFT)
-        b2.pack(side = RIGHT)
+        b2.pack(side = LEFT)
+        b3.pack(side = LEFT)
         
+
+        l1 = Label(f1, text="n=")
+        self.e1 = Entry(f1)
+        l1.pack(side=LEFT)
+        self.e1.pack(side=LEFT)
         
-        self.w = Canvas(self.frame, width=100, height=100)
+        self.w = Canvas(self.frame, width=220, height=220)
         self.w.pack(side=BOTTOM)
         
         self.drawBoard()        
@@ -38,29 +45,43 @@ class BoardApp:
         self.w.create_rectangle(x*20+1, y*20+1, x*20+19, y*20+19, fill="red", outline = "red")
         
     def drawBoard(self):
-        self.w.create_rectangle(0, 0, 100, 100, fill="white")
+        self.w.create_rectangle(0, 0, 220, 220, fill="white")
         i = 0
-        while(i<5): #draw horizontal lines
-            self.w.create_line(0, i*20, 100, i*20, fill="black")
+        while(i<11): #draw horizontal lines
+            self.w.create_line(0, i*20, 220, i*20, fill="black")
             i += 1
         i = 0
-        while(i<5): #draw vertical lines
-            self.w.create_line(i*20, 0, i*20, 100, fill="black")
+        while(i<11): #draw vertical lines
+            self.w.create_line(i*20, 0, i*20, 220, fill="black")
             i += 1
     
     def piece1(self):
         self.drawBoard()
-        self.piece = Knight(2, 2, "white", Board())
+        self.piece = Knight(5, 5, "white", Board())
         for x in self.piece.getMoveTable():
             self.drawHighlight(x)
         self.drawPiece(self.piece)
         
     def piece2(self):
         self.drawBoard()
-        self.piece = Rook(2, 2, "white", Board())
+        self.piece = Rook(5, 5, "white", Board())
         for x in self.piece.getMoveTable():
             self.drawHighlight(x)
         self.drawPiece(self.piece)
+        
+    def piece3(self):
+        try:
+            n = int(self.e1.get())
+        except:
+            n = 5
+        if(n>5 or n<1):
+            n = 5
+        self.drawBoard()
+        self.piece = Bomb(5, 5, "white", Board())
+        self.piece.timer = n
+        for x in self.piece.getMoveTable():
+            self.drawHighlight(x)
+        self.drawPiece(self.piece)        
         
         
 def board(): #init function - call to start
